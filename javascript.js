44b6820efbeabdc7d38094554379492f1799be5c
Anna Kviese <anna.kviese@gmail.com>
2016-10-07 14:09:11 -0700

    creating featured section with images in flexbox

---
+++ javascript.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+
+$('#submit').click(function(){
+          var email = $('#email').val();
+          var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          if re.test(email) {
+          		prompt ('Thanks for subscribing');
+          } else {
+          		alert ('Please enter valid email address!')          		
+          }
+      };
+});
+
+
+
+
+
+ });
+
